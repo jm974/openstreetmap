d5dfb1703d94202bb38a8db115e458120be65a88
jm974 <jmhuet@gmail.com>
2017-01-26 21:11:24 +0100

    Create OpenStreetMapTools.py

---
+++ OpenStreetMapTools.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import xml.etree.cElementTree as ET
+from collections import defaultdict
+import codecs
+import json
+import re
+import pprint
+
+def get_element(osm_file, tags=('node', 'way', 'relation')):
+    """Yield element if it is the right type of tag
+
+    Reference:
+    http://stackoverflow.com/questions/3095434/inserting-newlines-in-xml-file-generated-via-xml-etree-elementtree-in-python
+    """
+    context = iter(ET.iterparse(osm_file, events=('start', 'end')))
+    _, root = next(context)
+    for event, elem in context:
+        if event == 'end' and elem.tag in tags:
+            yield elem
+            root.clear()
